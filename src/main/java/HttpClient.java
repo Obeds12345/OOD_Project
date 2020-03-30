741a4cd53556a7a202c0fbae9385c480b04d6188
Obed_Osei <obeds12345@gmail.com>
2020-03-30 01:55:17 -0500

    Trying java http get request form github==> works

---
+++ src/main/java/HttpClient.java
@@ -0,0 +1,31 @@
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class HttpClient {
+    public static void HttpGet() {
+        String url = "https://api.github.com/users";
+
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet get = new HttpGet(url);
+        CloseableHttpResponse resp = null;
+        try {
+            resp = client.execute(get);
+            HttpEntity entity = resp.getEntity();
+            System.out.println("Json response");
+            System.out.println(EntityUtils.toString(entity));
+
+        } catch (IOException ioe) {
+            System.err.println("Something went wrong getting the weather: ");
+            ioe.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Unknown error: ");
+            e.printStackTrace();
+        }
+    }
+}
