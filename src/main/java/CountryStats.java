55f2c9b274474ac1958abad35cd0f403b0088dfc
Chris Gomez <chris@Chriss-MBP.hsd1.il.comcast.net>
2020-04-16 13:46:23 -0500

    Added new files to the src folder. Added a Json parser to the build.

---
+++ src/main/java/CountryStats.java
@@ -0,0 +1,71 @@
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class CountryStats {
+    private String countryName;
+    private Long numberOfEntries;
+    private Map<String, JSONObject> covidStatsByDateMap;
+
+    public CountryStats(String countryName) throws ParseException {
+        this.countryName = countryName;
+        HttpClient.specificDataForCountry(countryName,"");
+        this.covidStatsByDateMap = this.createMap();
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public Long getNumberOfEntries() {
+        return numberOfEntries;
+    }
+
+    public Map<String, JSONObject> getCovidStatsByDateMap() {
+        return covidStatsByDateMap;
+    }
+
+    private Map<String, JSONObject> createMap() throws ParseException {
+        Map<String, JSONObject> covidStatsByDateMap = new HashMap<>();
+
+        try {
+            // Check if file exists. Should be named "output.json"
+            Object obj = new JSONParser().parse(new FileReader("output" + File.separator + "output.json"));
+
+            // typecasting obj to JSONObject
+            JSONObject jo = (JSONObject) obj;
+
+            // getting count of entries and all results
+            this.numberOfEntries = (Long) jo.get("count");
+
+            // Get all results and transform them into our own HashMap<String, JsonObject>
+            HashMap results = (HashMap) jo.get("result");
+
+            // Iterate and add all values to our own hashmap
+            Iterator iter = results.entrySet().iterator();
+
+            while (iter.hasNext()) {
+                Map.Entry mapElement = (Map.Entry) iter.next();
+                covidStatsByDateMap.put(mapElement.getKey().toString(), (JSONObject) mapElement.getValue());
+            }
+
+            return covidStatsByDateMap;
+        } catch (FileNotFoundException e) {
+            System.out.println("File does not exist. Should be named output.json");
+        } catch (IOException e) {
+            System.out.println("IO exception");
+        }
+
+        return null;
+    }
+
+
+}
--- src/main/java/HttpClient.java
+++ src/main/java/HttpClient.java
@@ -5,15 +5,46 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class HttpClient {
     public static String apiUrl = "https://covidapi.info/api/v1/";
 
     public static void specificDataForCountry(String country, String date) {
         String url = date == "" ? apiUrl + "country/" + country : apiUrl + "country/" + country + "/" + date;
-        System.out.println(url);
-        HttpGet(url);
+
+        Path output = Paths.get("output");
+
+        //System.out.println(url);
+
+        String json = NewHttpGet(url);
+
+        if(!Files.exists(output.toAbsolutePath())) {
+            try {
+                Files.createDirectories(output.toAbsolutePath());
+                output = output.resolve("output.json");
+                BufferedWriter writer = Files.newBufferedWriter(output);
+                writer.write(json);
+                writer.close();
+            } catch (IOException e) {
+                System.out.println("Not there");
+            }
+        } else {
+            try {
+                output = output.resolve("output.json");
+                BufferedWriter writer = Files.newBufferedWriter(output);
+                writer.write(json);
+                writer.close();
+            } catch (IOException e) {
+                System.out.println("Not happening");
+            }
+        }
+
+
     }
 
     public static void golbalStats(String date1, String date2) {
@@ -35,6 +66,28 @@ public class HttpClient {
         HttpGet(url);
     }
 
+    // Obed, I made this one to play around with your method. Yours is below this one. Sorry if i messed it up a bit.
+    public static String NewHttpGet(String url) {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet get = new HttpGet(url);
+        CloseableHttpResponse resp = null;
+        try {
+            resp = client.execute(get);
+            HttpEntity entity = resp.getEntity();
+
+            String json = EntityUtils.toString(entity);
+
+            return json;
+        } catch (IOException ioe) {
+            System.err.println("Something went wrong getting the weather: ");
+            ioe.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Unknown error: ");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     public static void HttpGet(String url) {
         CloseableHttpClient client = HttpClients.createDefault();
         HttpGet get = new HttpGet(url);
@@ -44,7 +97,6 @@ public class HttpClient {
             HttpEntity entity = resp.getEntity();
             System.out.println("Json response");
             System.out.println(EntityUtils.toString(entity));
-
         } catch (IOException ioe) {
             System.err.println("Something went wrong getting the weather: ");
             ioe.printStackTrace();
